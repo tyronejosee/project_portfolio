a58198f551b25d37d2e92b58439a0e9c5f04b8de
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2023-12-31 21:25:20 -0300

    Add BEM methodology to Tailwind components, Add Space Grotesk font.

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,18 @@
+"""Models for Home App."""
+
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    """Entity type model for Project."""
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    repository = models.URLField()
+    website = models.URLField(null=True, blank=True)
+    image = models.FileField(upload_to='project/')
+    status = models.CharField(max_length=150)
+
+    def __str__(self):
+        return str(self.title)
+    
