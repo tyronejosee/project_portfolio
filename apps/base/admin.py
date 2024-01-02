2e49e0de3b1b6dd5cdf20b11f21f053ad36eb90a
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-02 15:47:12 -0300

    Add admin config for models.

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,3 +1,46 @@
+"""Admin configs for Base App."""
+
 from django.contrib import admin
 
-# Register your models here.
+from apps.base.models import (
+    Portfolio,
+    Project,
+    Skill,
+    WorkExperience,
+    Contact
+)
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    """Admin config for the Portfolio model."""
+    list_display = ('full_name', 'specialization',)
+    ordering = ('pk',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Admin config for the Project model."""
+    list_display = ('title', 'repository', 'status',)
+    ordering = ('pk',)
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    """Admin config for the Skill model."""
+    list_display = ('name', 'status',)
+    ordering = ('pk',)
+
+
+@admin.register(WorkExperience)
+class WorkExperienceAdmin(admin.ModelAdmin):
+    """Admin config for the WorkExperience model."""
+    list_display = ('job_title', 'company', 'status',)
+    ordering = ('pk',)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """Admin config for the Contact model."""
+    list_display = ('name', 'email', 'phone_number',)
+    ordering = ('pk',)
