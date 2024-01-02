e3ed8aa819e063cd99c3e3a14ea037c0be6c1ea8
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-02 18:08:51 -0300

    Add portfolio context processor and render all its data.

---
+++ apps/base/context_processors.py
@@ -0,0 +1,9 @@
+"""Context Processors for Base App."""
+
+from apps.base.models import Portfolio
+
+
+def portfolio_context(_request):
+    """Context processor for all data from the Portfolio model."""
+    portfolio = Portfolio.objects.first()
+    return {'portfolio': portfolio}
