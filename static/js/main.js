6ee948f2201b03339b159bd3ab8a4ea384635222
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-01-16 12:07:21 -0300

    Remove HTML and JS from theme toggle.

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleModeButton = document.getElementById('toggleModeButton');
-    const htmlElement = document.documentElement;
-    const darkModeStyles = document.getElementById('darkModeStyles');
-    let isDarkMode = true;
-
-    toggleModeButton.addEventListener('click', () => {
-        isDarkMode = !isDarkMode;
-
-        if (isDarkMode) {
-            htmlElement.classList.add('dark');
-            darkModeStyles.href = "/static/css/dark-mode.css";
-        } else {
-            htmlElement.classList.remove('dark');
-            darkModeStyles.href = "/static/css/light-mode.css";
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     const toggleHidden = document.getElementById('toggleHidden');
     const introSection = document.getElementById('intro-section');
 
