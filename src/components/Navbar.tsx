fcc66e11f3ac9b379646bf6f3886a3bf4235d280
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-04-30 12:11:27 -0400

    styles(pages): update input styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,10 +17,10 @@ export function Navbar() {
             <input
               type="search"
               id="search-navbar"
-              className="form__input-text text-center focus:text-left focus:pl-12 hover:bg-neutral-300 dark:hover:bg-neutral-700"
+              className="h-10 p-4 bg-neutral-200 dark:bg-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-700 focus:bg-neutral-300 dark:focus:bg-neutral-700 placeholder:text-neutral-600 w-full rounded-xl focus:outline-none focus:ring focus:ring-chartreuse-400 text-center focus:text-left focus:pl-12"
               placeholder="What topic are you interested in?"
               name="search"
-              value="#"
+              // value=""
             />
             <div className="absolute rounded-full inset-y-0 left-0 p-4 flex items-center pointer-events-none">
               <svg
@@ -42,8 +42,12 @@ export function Navbar() {
         </div>
 
         <div className="flex space-x-4">
-          <div className="flex space-x-4">
-            <button>Theme</button>
+          <div>
+            <label className="inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" />
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-chartreuse-400 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-chartreuse-800"></div>
+              <span className="ms-3 text-sm font-medium">Theme</span>
+            </label>
           </div>
           <a
             href="#cv_link"
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
 import { FULL_NAME, ABOUT_ME } from "../constants";
 
+import projectsData from "../data/projects.json";
+import skillsData from "../data/skills.json";
+
 export function HomePage() {
+  const activeProjects = projectsData.filter((project) => project.status);
+  const activeSkills = skillsData.filter((skill) => skill.status);
+
   return (
     <>
+      {/* About Section */}
       <section className="section">
         <h2>
           <span className="section__title">About</span>
@@ -13,23 +20,55 @@ export function HomePage() {
         </article>
       </section>
 
+      {/* Project Section */}
       <section className="section">
         <h2>
           <span className="section__title">Projects</span>
         </h2>
-        <article className="project__grid">
-          <figure className="project__img">
-            <img src="" alt="" className="object-cover w-full h-full" />
-          </figure>
-          <div className="overflow-x-clip">
-            <h3 className="text__bold">Project title</h3>
-            <span className="block">Project description</span>
-            <span className="block">Project repository</span>
-            <span className="block">Project website</span>
-          </div>
-        </article>
+        {activeProjects.map((project) => (
+          <article key={project.id} className="project__grid">
+            <figure className="project__img">
+              <img
+                src={project.image}
+                alt={project.name}
+                className="object-cover w-full h-full"
+              />
+            </figure>
+            <div className="overflow-x-clip">
+              <h3 className="text__bold">{project.name}</h3>
+              <p className="block">{project.description}</p>
+              <a
+                href={project.repository}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-neutral-500"
+              >
+                {project.repository}
+              </a>
+              <a
+                href={project.website}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-neutral-500"
+              >
+                {project.website}
+              </a>
+              <div className="flex space-x-2">
+                {project.skills.map((skill, index) => (
+                  <span
+                    key={index}
+                    className="block text-xs font-medium border rounded py-1 px-2"
+                  >
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </article>
+        ))}
       </section>
 
+      {/* Experiences Section */}
       <section className="section">
         <h2>
           <span className="section__title">Experiences</span>
@@ -53,18 +92,22 @@ export function HomePage() {
         </article>
       </section>
 
+      {/* Skills Section */}
       <section className="section" id="skills-section">
         <h2>
           <span className="section__title">Skills</span>
         </h2>
         <section className="skill__grid">
-          <article className="p-4 h-28 flex flex-col justify-center items-center bg-neutral-200 dark:bg-neutral-800 hover:bg-chartreuse-400 hover:text-neutral-900 rounded-xl hover-scale transform hover:scale-105 group space-y-2">
-            Skill icon
-            <span className="text-xs font-bold">Skill name</span>
-          </article>
+          {activeSkills.map((skill) => (
+            <article className="p-4 h-28 flex flex-col justify-center items-center bg-neutral-200 dark:bg-neutral-800 hover:bg-chartreuse-400 hover:text-neutral-900 rounded-xl hover-scale transform hover:scale-105 group space-y-2">
+              <img src={skill.icon} alt={skill.name} />
+              <span className="text-xs font-bold">{skill.name}</span>
+            </article>
+          ))}
         </section>
       </section>
 
+      {/* Contact Section */}
       <section className="section">
         <h2>
           <span className="section__title">Contact</span>
@@ -72,15 +115,15 @@ export function HomePage() {
         <form method="POST" action="" className="form">
           <label className="form__label">
             <p className="text__bold">Name:</p>
-            <input type="text" />
+            <input type="text" className="form__input form__input--text" />
           </label>
           <label className="form__label">
             <p className="text__bold">Email:</p>
-            <input type="text" />
+            <input type="email" className="form__input form__input--text" />
           </label>
           <label className="form__label">
             <p className="text__bold">Message:</p>
-            <input type="text" />
+            <input type="text" className="form__input form__input--textarea" />
           </label>
           <div className="flex space-x-4">
             <button type="reset" className="btn btn--secondary w-full">
