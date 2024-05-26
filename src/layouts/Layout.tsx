57ae7b0750320433d18128f42b950ceba6d83904
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-05-25 20:03:55 -0400

    ci: add github actions and github pages configs

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -13,6 +13,7 @@ export function Layout({ children }: LayoutProps) {
       <main className="max-w-screen-lg mx-auto space-y-16 flex flex-col min-h-[650px] p-4">
         {children}
       </main>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       {/* <ScrollToTop /> */}
     </>
   );
