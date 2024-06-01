0b4a142dec5487418e5eba7c0869956c4c5b46b1
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-06-01 19:01:40 -0400

    feat: add data and styles to bookmarks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Layout } from "./layouts/Layout";
 import { HomePage } from "./pages/HomePage";
