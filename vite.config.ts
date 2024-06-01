58d4572242311a5e050d534e7b6240da31db4855
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-06-01 15:51:28 -0400

    fix: tests paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://tyronejosee.github.io/",
-  //base: "https://tyronejosee.github.io/project_portfolio",
+  base: "https://tyronejosee.github.io/project_portfolio",
 });
